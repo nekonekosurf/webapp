0fcd5bfe5241a9701a46cfa02e4e7b260cf257e6
nekonekosurf <nekonekosurf88@gmail.com>
2019-11-07 20:20:15 +0900

    課題3ー1終了だが発展できてない

--- webapp/1stWeek/ex310.js
+++ webapp/1stWeek/ex310.js
@@ -19,49 +19,47 @@ function watchStart(){
   if (! isStarted) {
     // Date() はミリ秒単位の現在時刻を出力する関数
     start = new Date();
-
     // setIntervalは第２引数で指定した間隔（ミリ秒単位）ごとに
     // 第１引数で指定した関数を呼び出す関数
     timer = setInterval(updateWatch, 1000/60);
     // ここでは 1/60 秒に１度 updateWatch を呼び出すと指定
-
     isStarted = true;
   }
 }
-
 //停止ボタンのイベントハンドラー（仕様(7)を満たすためには改造が必要）
 function watchStop(){
   if ( isStarted ) {
-
     // clearIntervalはsetIntervalを停止する関数
     clearInterval(timer);
     isStarted = false;
   }
 }
-
 //リセットボタンのイベントハンドラー（仕様(6)を満たすためには改造が必要）
 function watchReset(){
   watchStop();
-
   // DOM の watch ノードのHTML記述を更新することでゼロにリセット
   watch.innerHTML = "00:00:00:0";
 }
-
 //計測中の時刻計算用関数
 function updateWatch() {
   //現在時間を date に代入
   var date = new Date();
-
   //ここで diff という変数を定義し、現在時刻とスタートした時刻の差分を代入
+  diff = date.getTime()-start.getTime();
   //するコードを以下に書くこと。具体的には date と start オブジェクトの
   //getTime() メソッドの返り値を用いて計算すること。 getTime() メソッドは、
   //オブジェクト内に格納されているミリ秒単位の時刻を出力する。
-
   //時、分、秒、デシ秒(1/10秒)をそれぞれ計算するコードを以下に書くこと。
-
-  //表示用に桁数を合わせるコードを以下に書くこと。
-
-  //DOMの watch ノードのHTML記述を書き換えることでスタートからの経過時間を
+  diff = diff/1000;
+  hour = Math.floor(diff/3600);
+  diff = diff-hour*3600;
+  min = Math.floor(diff/60);
+  diff = diff - min*60;
+  sec =Math.floor(diff);
+  diff = diff -sec;
+  deci = Math.floor(diff*10);
+  // //表示用に桁数を合わせるコードを以下に書くこと。
+  //DOMの watch ノードのHTML記述を書き換えることでスタートかSらの経過時間を
   //表示するコードを以下に書くこと。
-
+  watch.innerHTML=Math.floor(hour)+":"+Math.floor(min)+":"+Math.floor(sec)+":"+deci
 }
